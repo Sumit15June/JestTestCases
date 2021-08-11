fc00cc3102656c1f738aeb6f39a25a3289d06bc9
Sumit15June <sumit.saurabh@tarento.com>
2021-08-11 09:24:31 +0530

    firstcommit

---
+++ function.js
@@ -0,0 +1,22 @@
+//onst axios = require("axios");
+
+const functions = {
+    add: (number1, number2) => number1 + number2,
+    isNull: () => null,
+    checkValue: (x) => x,
+    createUser: () => {
+        const user = { firstName: 'Sumit' };
+        user['lastName'] = 'Saurabh';
+        return user;
+    },
+
+    fetchUserData: () =>
+        axios
+            .get("https://jsonplaceholder.typicode.com/users/1")
+            .then(res => res.data)
+            .catch(err => "error")
+
+
+}
+
+module.exports = functions;
---
+++ function.test.js
@@ -0,0 +1,63 @@
+   // tobe Value
+const functions=require("./function");
+
+test("ADD 2 and 2 to be equal to 4",()=>{
+    expect(functions.add(2,2)).toBe(4);
+})
+
+
+
+//not to be Value
+test("ADD 2 and 2 to be not equal to 5",()=>{
+    expect(functions.add(2,2)).not.toBe(5);
+})
+
+
+//checking for truth and false values
+test("should be a null value",()=>{
+    expect(functions.isNull()).toBeNull();
+});
+
+//check for a value to be false or not
+test("should be falsy",()=>{
+    expect(functions.checkValue(0)).toBeFalsy();
+});
+
+//check for the user object
+test("user should be Sumit Saurabh object",()=>{
+    expect(functions.createUser()).toEqual({
+        firstName:"Sumit",
+        lastName:"Saurabh"
+    });
+});
+
+
+//check for greater and les than values
+test("should be less than 1400",()=>{
+    const num1=400;
+    const num2=700;
+    expect(num1+num2).toBeLessThan(1400);
+})
+
+//Regular Expression
+test("There is no I in captain",()=>{
+    expect('captain').not.toMatch(/I/);
+})
+
+
+//Arrays
+test('work should be in employee',()=>{
+    employee=["lunch","party","work","meetings"];
+    expect(employee).toContain("work");
+})
+
+//working with async data
+// test("user fetched name should be Leanne Graham",()=>{
+//     expect.assertions(1)
+//     return functions.fetchUserData()
+//     .then(data=>{expect(data.name).toEqual("Leanne Graham")})
+// })
+
+
+
+
