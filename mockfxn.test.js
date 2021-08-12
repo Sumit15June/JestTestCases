daec344822273110c9b291f650dfd454ada495c9
Sumit15June <sumit.saurabh@tarento.com>
2021-08-12 16:04:53 +0530

    early concepts of mock with Fetch

--- mockfxn.test.js
+++ mockfxn.test.js
@@ -5,9 +5,9 @@ const axios=require("axios");
 //https://swapi.dev/api/people
 
 //get a star wars person by using its ID
-const warperson=(id)=>{
-    axios.get(`https://swapi.dev/api/people/${id}/`)
-    .then((res)=>{res.data.name})
+function warperson(id){
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}/`)
+    .then((res)=>res.title)
     .catch((err)=>{
         console.error(err)
     })
@@ -17,9 +17,9 @@ const warperson=(id)=>{
 
 
 describe("star wars", ()=>{
-    test("test to return a name", ()=>{
-        const result=warperson(1);
+    test("test to return a name",async()=>{
+        const result= await warperson(1);
         console.log(result);
-        expect(result).toBe("Luke Skywalker")
+        expect(result).toBe("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")
     })
 })
